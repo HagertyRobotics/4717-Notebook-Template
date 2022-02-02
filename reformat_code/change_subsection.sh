42705dbb747ebaf589827d077a00e9fa8176c8a8
HagertyRobotics <crabmeat99@gmail.com>
2022-02-02 17:14:23 -0500

    Change subsection to hhscommittee

--- reformat_code/change_subsection.sh
+++ reformat_code/change_subsection.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 sed -Ei -e 's/\subsection\*{General}/\hhscommittee{General}/g' $1
+sed -Ei -e 's/\subsection\*{Hardware}/\hhscommittee{Hardware}/g' $1
+sed -Ei -e 's/\subsection\*{Multimedia}/\hhscommittee{Multimedia}/g' $1
+sed -Ei -e 's/\subsection\*{Outreach}/\hhscommittee{Outreach}/g' $1
+sed -Ei -e 's/\subsection\*{Software}/\hhscommittee{Software}/g' $1
+sed -Ei -e 's/\subsection\*{Strategy}/\hhscommittee{Strategy}/g' $1
+
