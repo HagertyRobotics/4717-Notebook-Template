97f6287736c8ed6fd2a3c94f44e311f3c58224d7
175Samantha175 <70240410+175Samantha175@users.noreply.github.com>
2023-06-09 16:13:08 -0400

    final notebook template

--- reformat_code/change_subsection.sh
+++ reformat_code/change_subsection.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sed -Ei -e 's/\subsection\*{General}/\hhscommittee{General}/g' $1
-sed -Ei -e 's/\subsection\*{Hardware}/\hhscommittee{Hardware}/g' $1
-sed -Ei -e 's/\subsection\*{Multimedia}/\hhscommittee{Multimedia}/g' $1
-sed -Ei -e 's/\subsection\*{Outreach}/\hhscommittee{Outreach}/g' $1
-sed -Ei -e 's/\subsection\*{Software}/\hhscommittee{Software}/g' $1
-sed -Ei -e 's/\subsection\*{Strategy}/\hhscommittee{Strategy}/g' $1
+sed -Ei -e 's/\subsection\*{General}/\committee{General}/g' $1
+sed -Ei -e 's/\subsection\*{Hardware}/\committee{Hardware}/g' $1
+sed -Ei -e 's/\subsection\*{Multimedia}/\committee{Multimedia}/g' $1
+sed -Ei -e 's/\subsection\*{Outreach}/\committee{Outreach}/g' $1
+sed -Ei -e 's/\subsection\*{Software}/\committee{Software}/g' $1
+sed -Ei -e 's/\subsection\*{Strategy}/\committee{Strategy}/g' $1
 
